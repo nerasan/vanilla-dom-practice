94465ea45fb955dc4ac577c54b84a50f07f383e5
nerasan <salima.harun@gmail.com>
2020-09-07 00:34:30 -0500

    changed color of one li element

--- dom-practice.js
+++ dom-practice.js
@@ -10,3 +10,7 @@ h2.innerText = 'Welcome to Houston, Texas!!!'
 
 const container = document.querySelectorAll('.container')
 console.log(container)
+
+const lists = document.querySelectorAll('li')
+console.log(lists)
+lists[3].style.color = '#3399ff' // changes the 3rd index of lists which is the 4th down the list
